034218d131a989f8b80d48632ce26e4e32232f4a
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-18 14:23:33 +0300

    18.07.2023

--- src/test/java/Get04b.java
+++ src/test/java/Get04b.java
@@ -1,2 +1,21 @@
 public class Get04b {
+     /*
+    Given
+        https://reqres.in/api/users/2
+    When
+        user send GET Request to the url
+    Then
+        HTTP Status code should be 200
+    And
+        Response format should be "application/json"
+    And
+        "email" is "janet.weaver@reqres.in"
+     And
+        "first_name" is "Janet"
+     And
+        "last_name" is "Weaver"
+     And
+        "text" is "To keep ReqRes free, contributions towards server costs are appreciated!"
+
+     */
 }
