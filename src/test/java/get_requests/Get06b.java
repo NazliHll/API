36aef4d19852acc396a1f4c24d3c1ecf41842305
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-18 16:02:18 +0300

    18.07.2023

---
+++ src/test/java/get_requests/Get06b.java
@@ -0,0 +1,20 @@
+package get_requests;
+
+public class Get06b {
+
+     /*
+    Given
+          https://reqres.in/api/unknown
+    When
+          I send GET request to the URL
+    Then
+
+          1) Status code is 200
+          2) Print all pantone_values
+          3) Print all ids greater tahen 3 on the console
+             Assert that there are 3 ids greater than 3
+          4) Print all names whose ids are less than 3 on the console
+             Assert that the number of names whose ids are less than 3 is 2
+     */
+
+}
