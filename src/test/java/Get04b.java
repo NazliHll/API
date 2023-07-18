cd050f583c936d823e98cb4ded6ea8399f56b96b
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-18 14:24:33 +0300

    18.07.2023

--- src/test/java/Get04b.java
+++ src/test/java/Get04b.java
@@ -1,21 +1,14 @@
 public class Get04b {
-     /*
-    Given
-        https://reqres.in/api/users/2
-    When
-        user send GET Request to the url
-    Then
-        HTTP Status code should be 200
-    And
-        Response format should be "application/json"
-    And
-        "email" is "janet.weaver@reqres.in"
-     And
-        "first_name" is "Janet"
-     And
-        "last_name" is "Weaver"
-     And
-        "text" is "To keep ReqRes free, contributions towards server costs are appreciated!"
+      /*
+        Given
+            https://restful-booker.herokuapp.com/booking?firstname=John&lastname=Smith
+        When
+            User sends get request to the URL
+        Then
+            Status code is 200
+	  	And
+	  		Among the data there should be someone whose firstname is "John" and lastname is "Smith"
 
      */
+
 }
