c8c0ce2c4dec81bcb6aa11960e078e5bfe75259e
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-18 17:43:09 +0300

    18.07.2023

--- src/test/java/get_requests/Get07b.java
+++ src/test/java/get_requests/Get07b.java
@@ -1,5 +1,32 @@
 package get_requests;
 
-public class Get07b {
+import base_urls.JsonPlaceHolderBaseUrl;
+import org.junit.Test;
+
+public class Get07b extends JsonPlaceHolderBaseUrl {
+     /*
+         Given
+            https://jsonplaceholder.typicode.com/todos/2
+        When
+            I send GET Request to the URL
+        Then
+            Status code is 200
+            And "completed" is false
+            And "userId" is 1
+            And "title" is "quis ut nam facilis et officia qui"
+            And header "Via" is "1.1 vegur"
+            And header "Server" is "cloudflare"
+            {
+                "userId": 1,
+                "id": 2,
+                "title": "quis ut nam facilis et officia qui",
+                "completed": false
+            }
+     */
+    @Test
+    public void test07(){
+        spec.pathParams("first","todos","second",2);
+
+    }
 
 }
