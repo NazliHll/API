3bb5caa4691f72f7ab887f733332765de6a91be4
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-24 14:17:48 +0300

    24.07.2023

--- src/test/java/get_requests/Get08.java
+++ src/test/java/get_requests/Get08.java
@@ -1,4 +1,23 @@
 package get_requests;
 
 public class Get08 {
+     /*
+      Given
+          https://restful-booker.herokuapp.com/booking/1666
+      When
+          I send GET Request to the url
+      Then
+          Response body should be like that;
+       {
+    "firstname": "Sally",
+    "lastname": "Brown",
+    "totalprice": 111,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2013-02-23",
+        "checkout": "2014-10-23"
+    },
+    "additionalneeds": "Breakfast"
+}
+   */
 }
