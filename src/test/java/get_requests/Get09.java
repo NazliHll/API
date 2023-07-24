fe6c198c36fd0962cbf4afb91ac1b8f20f773a09
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-24 15:07:29 +0300

    24.07.2023

---
+++ src/test/java/get_requests/Get09.java
@@ -0,0 +1,25 @@
+package get_requests;
+
+public class Get09 {
+     /*
+        Given
+            https://gorest.co.in/public/v1/users/2986
+        When
+            User send GET Request to the URL
+        Then
+            Status Code should be 200
+        And
+            Response body should be like
+            /*
+            {
+        "meta": null,
+        "data": {
+            "id": 2986,
+            "name": "Navin Talwar",
+            "email": "navin_talwar@mclaughlin.name",=>burası inner class
+            "gender": "male",
+            "status": "inactive"
+              }
+         }
+     */
+}
