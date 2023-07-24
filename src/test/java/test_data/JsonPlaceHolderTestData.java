80f144671c79f8b475a0839b14df851f83a50ed6
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-24 13:08:16 +0300

    24.07.2023

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -1,4 +1,14 @@
 package test_data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JsonPlaceHolderTestData {
+    public Map<String,Object> expectedData(Integer userId,String title,Boolean completed){
+
+        Map<String, Object> expectedData=new HashMap<>();
+        expectedData.put("userId",1);
+        expectedData.put("title","quis ut nam facilis et officia qui");
+        expectedData.put("completed",false);
+    }
 }
