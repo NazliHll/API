5cb3dfd6c10649a2cc1443603925fd3b891a8e14
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-24 16:01:18 +0300

    24.07.2023

--- src/test/java/get_requests/Get09.java
+++ src/test/java/get_requests/Get09.java
@@ -1,9 +1,20 @@
 package get_requests;
 
-public class Get09 {
+import base_urls.GoRestBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.GoRestTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get09 extends GoRestBaseUrl {
      /*
         Given
-            https://gorest.co.in/public/v1/users/2986
+            https://gorest.co.in/public/v1/users/3810853
         When
             User send GET Request to the URL
         Then
@@ -14,12 +25,38 @@ public class Get09 {
             {
         "meta": null,
         "data": {
-            "id": 2986,
-            "name": "Navin Talwar",
-            "email": "navin_talwar@mclaughlin.name",=>burası inner class
+             "id": 3810853,
+            "name": "Susheel Khatri",
+            "email": "susheel_khatri@parker-vonrueden.test",
             "gender": "male",
             "status": "inactive"
               }
          }
      */
+    @Test
+    public void test09(){
+        spec.pathParams("first","users","second",3810853);
+
+        GoRestTestData obj=new GoRestTestData();
+        Map<String,String > dataKeyMap=obj.dataKeyMap("Susheel Khatri","susheel_khatri@parker-vonrueden.test","male","inactive");
+        Map<String,Object> expectedData=obj.expectedDataMethod(null,dataKeyMap);
+        System.out.println(expectedData);
+
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        Map<String,Object>actualData=response.as(HashMap.class);
+        System.out.println(actualData);
+        assertEquals(expectedData.get("meta"),actualData.get("meta"));
+        assertEquals(dataKeyMap.get("name"),((Map)actualData.get("data")).get("name"));
+        assertEquals(dataKeyMap.get("email"),((Map)actualData.get("data")).get("email"));
+        assertEquals(dataKeyMap.get("gender"),((Map)actualData.get("data")).get("gender"));
+        assertEquals(dataKeyMap.get("status"),((Map)actualData.get("data")).get("status"));
+        assertEquals(200,response.statusCode());
+
+
+
+
+    }
+
 }
