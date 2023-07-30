5786e9d7d4912576c124fdb6029c3bf8eac23418
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-30 15:11:42 +0300

    30.07.2023

---
+++ src/test/java/API_Practice/GetRequest03.java
@@ -0,0 +1,17 @@
+package API_Practice;
+
+public class GetRequest03 {
+    /*
+    //Matchers yöntemi ile(Matchers.equalTo)
+    id'si 5 olanın datalarınının aşağıdaki gibi olduğunu test ediniz
+    "https://reqres.in/api/users"
+    {
+        "id": 5,
+            "email": "charles.morris@reqres.in",
+            "first_name": "Charles",
+            "last_name": "Morris",
+            "avatar": "https://reqres.in/img/faces/5-image.jpg"
+    }
+
+     */
+}
