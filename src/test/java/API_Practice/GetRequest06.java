7128d75b105df630ca769660468158c72f0afd77
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-30 22:45:58 +0300

    30.07.2023

--- src/test/java/API_Practice/GetRequest06.java
+++ src/test/java/API_Practice/GetRequest06.java
@@ -1,10 +1,12 @@
 package API_Practice;
 
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import utilities.Authentication;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetRequest06 extends Authentication {
     @Test
@@ -13,5 +15,11 @@ public class GetRequest06 extends Authentication {
 
         Response response=given().headers("Authorization","Bearer "+generateToken()).when().get(url);
         response.prettyPrint();
+
+        //Matcher Class ile müşteri bilgilerini doğrulayalım
+        response.then().assertThat().body("firstName", equalTo("Deneme"),
+                "lastName",equalTo("Last"),
+                "email",equalTo("ma34@gmail.com"),
+                "mobilePhoneNumber",equalTo("123-123-3434"));
     }
 }
