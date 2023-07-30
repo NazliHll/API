c63b78daf1f3d1657f7070f3b359be6bd0a8b2e6
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-30 22:08:09 +0300

    30.07.2023

---
+++ src/test/java/API_Practice/GetRequest05.java
@@ -0,0 +1,17 @@
+package API_Practice;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest05 {
+    @Test
+    public void test05(){
+        String url="https://www.gmibank.com/api/tp-customers";
+        String token="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRjaDgxIiwiYXV0aCI6IlJPTEVfQURNSU4iLCJleHAiOjE2ODkzNzEzNjF9.HMSRfnM7XDebs7_g77wO4RgZgw51MeHejMx64NWFr5SWDjwnQ73Qi8GHW-n-gV9ha3gECMXJEgmNGHAcELMWQA";
+
+        Response response =given().headers("Authorization","Bearer "+token).when().get(url);
+        response.prettyPrint();
+    }
+}
---
+++ src/test/java/utilities/Authentication.java
@@ -0,0 +1,23 @@
+package utilities;
+
+public class Authentication {
+     /*
+        public static void main(String[] args)
+        {
+            String guncelToken = generateToken();
+            System.out.println(guncelToken);
+        }
+    */
+     public static String generateToken() {
+
+         String username = "Batch81";
+         String password = "Batch81+";
+
+        /*Api post body for authenticate=>postman
+        {
+            "password": "string",
+            "rememberMe": true,
+            "username": "string"
+        }
+         */
+}
