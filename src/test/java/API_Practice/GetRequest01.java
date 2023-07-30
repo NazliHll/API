c43f04db4a27c06bcf7f2f27e2a628631ca9a1a0
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-30 14:18:41 +0300

    30.07.2023

--- src/test/java/API_Practice/GetRequest01.java
+++ src/test/java/API_Practice/GetRequest01.java
@@ -1,10 +1,19 @@
 package API_Practice;
 
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class GetRequest01 {
     @Test
     public void test01(){
+
+        String url="https://restful-booker.herokuapp.com/booking";
+        Response response= given().when().get(url);
+        // given().when().get(url) -> end point'e göndermek için request oluşturmuş olduk.
+        // Response response -> api tarafından bana dönen response (cevap)
+
 
     }
 }
