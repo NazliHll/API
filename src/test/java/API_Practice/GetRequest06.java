40ab1b616b52ebdec251e09b750cc49fe1a44700
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-07-30 22:54:29 +0300

    30.07.2023

--- src/test/java/API_Practice/GetRequest06.java
+++ src/test/java/API_Practice/GetRequest06.java
@@ -1,12 +1,15 @@
 package API_Practice;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 import utilities.Authentication;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class GetRequest06 extends Authentication {
     @Test
@@ -21,5 +24,13 @@ public class GetRequest06 extends Authentication {
                 "lastName",equalTo("Last"),
                 "email",equalTo("ma34@gmail.com"),
                 "mobilePhoneNumber",equalTo("123-123-3434"));
+
+        //JsonPath ile müşteri bilgilerini doğrulama
+        JsonPath json=response.jsonPath();
+        assertEquals("Deneme",json.getString("firstName"));
+        assertEquals("Last",json.getString("lastName"));
+        assertEquals("ma34@gmail.com",json.getString("email"));
+        assertEquals("123-123-3434",json.getString("mobilePhoneNumber"));
+
     }
 }
